8c1f033bbead34a157fc4f36479b0fde471cf095
Daegyu Han <bigstar77han@gmail.com>
2026-02-10 00:06:11 +0900

    [FIX] vercel 배포 준비

--- app/trash/page.tsx
+++ app/trash/page.tsx
@@ -1,17 +1,26 @@
-'use client'
+"use client"
+import { useState } from 'react'
 import Header from '../../components/Header'
 import Sidebar from '../../components/Sidebar'
 import TrashContent from '../../components/TrashContent'
+import { TAGS } from '../../lib/tags'
 
 export default function TrashPage() {
+  const [sidebarOpen, setSidebarOpen] = useState(true)
+  const [selectedTags, setSelectedTags] = useState<string[]>(() => Object.keys(TAGS))
+
   return (
     <div className="min-h-screen bg-gray-50">
-      <Header />
+      <Header
+        onToggleSidebar={() => setSidebarOpen((s) => !s)}
+        sidebarOpen={sidebarOpen}
+        onHomeClick={() => {}}
+      />
       <div className="flex">
-        <aside className="w-64">
-          <Sidebar />
+        <aside className={`fixed left-0 top-16 bottom-0 w-64 transition-transform duration-500 ease-in-out ${sidebarOpen ? 'translate-x-0' : '-translate-x-full'}`}>
+          <Sidebar selectedTags={selectedTags} onTagChange={setSelectedTags} onHomeClick={() => {}} />
         </aside>
-        <main className="flex-1 p-6">
+        <main className={`flex-1 py-6 pr-6 transition-[padding-left] duration-500 ease-in-out ${sidebarOpen ? 'pl-[18rem]' : 'pl-6'}`}>
           <h2 className="text-2xl font-semibold">휴지통</h2>
           <div className="mt-4">
             <TrashContent />
